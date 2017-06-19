5eb2eefe55453febc33b8ad7d1bae47d36081d87
Obscurant999 <alexey.kozyrev@gmail.com>
2017-06-19 06:14:56 +0300

    Initial Committ

---
+++ src/org/library/Main.java
@@ -0,0 +1,15 @@
+package org.library;
+
+/**
+ * Created by kano on 18.06.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Books books = new Books();
+
+
+
+    }
+}
